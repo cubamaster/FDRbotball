e2c42e1009fdc1b9b8474b4473d2070b85f28827
cubamaster <muratkhid1129@gmail.com>
2022-04-27 14:13:38 -0400

    fixed the follow_line function

--- main.c
+++ main.c
@@ -5,27 +5,30 @@
 int rw = 0; // right wheel
 int lw = 1; // left wheel 
 int down = 2047; // stadnard lowest position for the lifting servo
-int up = 1740; // standard upper position for the lifting servo 
-int open = 600; // standard open position for the claw 
-int close = 1248; // standard closed open position for the claw
+int up = 1450; // standard upper position for the lifting servo 
+int open = 1630; // standard open position for the claw 
+int close = 2007; // standard closed open position for the claw
 int claw = 2; // claw servo- port 2
 int lift = 1; // lifting servo - port 1 
 int grab = 0; // grabbing sevro - port 0
 int touch1 = 0; // digital touching sensor - port 0
 int touch2 = 1; // digital touching sensor - port 1
 // function to follow the line forward. 
-void follow_line_1 (double coefficient) {
-    while(1 == 1) {
-        if (analog(0) > 3000) { // case if robot went over the black tape 
+void follow_line_1 (double coefficient, int position) {
+    cmpc(rw);
+    cmpc(lw);
+    int i = 0;
+    while(i <= position) {
+        if (analog(0) < 3000) { // case if robot went over the black tape 
             motor(rw, 100 * coefficient);
             motor(lw, -100);
         } // close if function 
         
-        else if(analog(0) < 3000) { // case if robot went over the white board
+        else if(analog(0) > 3000) { // case if robot went over the white board
             motor(rw, 100);
             motor(lw, -100 * coefficient);
         } // close if function
-    
+    i = gmpc(rw);
     }
 
 }
@@ -33,7 +36,7 @@ void follow_line_1 (double coefficient) {
 // function to follow the line backwards. 
 void follow_line_2 (double coefficient) {
     while(1 == 1) {
-        if (analog(0) > 3000) { // case if robot went over the black tape 
+        if (analog(2) > 3000) { // case if robot went over the black tape 
             motor(rw, -100);
             motor(lw, 100 * coefficient);
         } // close if function 
@@ -54,12 +57,13 @@ int main()
     motor(rw, 20);
     msleep(2000);
     
-    follow_line_1(0.7);
-    msleep(20000);
+    follow_line_1(0.7, 12000);
+    
     
     set_servo_position(claw, close);
     set_servo_position(lift, up);
     
+    /**
     while(get_digital_value(touch1) != 1) {
     	follow_line_1(0.7);
     }
@@ -87,5 +91,6 @@ int main()
 
     disable_servos();
     ao();
+    **/
     return 0;
 } // program ends
